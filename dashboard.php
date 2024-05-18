95b5fcfc19d6022aa4d9a28af936b6cd34748a18
JayaRaghul14 <jackraghul14092005@gmail.com>
2024-05-18 13:26:35 +0530

    commit

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,143 @@
 <!-- try changing the file -->
+
+<style>
+    <?php include 'CSS/main.css'; ?>
+	/* .new:hover{
+        transform: scale(1.3);
+	}
+	.new{
+		transition: transform .2s;
+	}
+	.cen{
+		text-align:center;
+	} */
+    .main{
+        padding-left: 50px;
+    }
+    .row{
+        text-align:center;
+    }
+</style>
+<body>
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header cen">Dashboard</h1>
+			</div>
+		</div>
+		
+		<div class="panel panel-container">
+			<div class="row">
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding new">
+					<div class="panel panel-teal panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-bed color-blue"></em>
+							<div class="large"></div>
+							<div class="text-muted"><h5><b>Chairs</b></h5></div>
+                            <img src="pic/chair.png" alt="">
+						</div>
+					</div>
+				</div>
+
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding new ">
+					<div class="panel panel-blue panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-bookmark color-orange"></em>
+							<div class="large"></div>
+							<div class="text-muted"><h5><b>Air Conditioner</h5></b></div>
+                            <img src="pic/ac.png" alt="">
+						</div>
+					</div>
+				</div>
+
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding new ">
+					<div class="panel panel-orange panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-users color-teal"></em>
+							<div class="large"></div>
+							<div class="text-muted"><h5><b>Lights</h5></b></div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding new ">
+					<div class="panel panel-red panel-widget ">
+						<div class="row no-padding"><em class="fa fa-xl fa-comments color-red"></em>
+							<div class="large"></div>
+							<div class="text-muted"><h5><b>Tables</h5></b></div>
+						</div>
+					</div>
+				</div>
+			</div><!--/.row-->
+
+			<hr>
+
+			<div class="row">
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding new">
+					<div class="panel panel-teal panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-reorder color-red"></em>
+							<div class="large"></div>
+							<div class="text-muted"><h5><b>Systems</h5></b></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding new">
+					<div class="panel panel-blue panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-check-circle color-green"></em>
+							<div class="large"></div>
+							<div class="text-muted"><h5><b>Labs</h5></b></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding new">
+					<div class="panel panel-orange panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-check-square-o color-magg"></em>
+							<div class="large"></div>
+							<div class="text-muted"><h5><b>Staff rooms</h5></b></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding new">
+					<div class="panel panel-red panel-widget ">
+						<div class="row no-padding"><em class="fa fa-xl fa-spinner color-blue"></em>
+							<div class="large"></div>
+							<div class="text-muted"><h5><b>Total Coimplaints</h5></b></div>
+						</div>
+					</div>
+				</div>
+			</div><!--/.row-->
+
+			<hr>
+
+			<div class="row">
+				<div class="col-xs-6 col-md-2 col-lg-2 no-padding">
+					
+				</div>
+
+				<div class="col-xs-6 col-md-4 col-lg-4 no-padding new">
+					<div class="panel panel-red panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-money color-red"></em>
+							<div class="large"></div>
+							<div class="text-muted"><h5><b>Resolved Issues</h5></b></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-4 col-lg-4 no-padding new">
+					<div class="panel panel-orange panel-widget ">
+						<div class="row no-padding"><em class="fa fa-xl fa-credit-card color-purp"></em>
+							<div class="large">&#8377; </div>
+							<div class="text-muted"><h5><b>Pending Payment</h5></b></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-2 col-lg-2 no-padding">
+					
+				</div>
+			</div><!--/.row-->
+		</div>
+	    
+	</div>	<!--/.main-->
+	
+
+		
+</body>
+</html>
