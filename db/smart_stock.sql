39093726753f4a23722717ebd9e98ec3e1c48183
Niranjan <niranjank.desktop@gmail.com>
2024-05-22 03:07:26 +0530

    Modified the .sql file which have the unused tables dropped

--- db/smart_stock.sql
+++ db/smart_stock.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2024 at 02:03 PM
+-- Generation Time: May 21, 2024 at 11:36 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -63,27 +63,6 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `availability`
---
-
-CREATE TABLE `availability` (
-  `item_id` int(11) NOT NULL,
-  `location_id` int(11) NOT NULL,
-  `no_of_items` int(11) NOT NULL,
-  `curr_condition` varchar(25) NOT NULL DEFAULT '"WORKING"'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `availability`
---
-
-INSERT INTO `availability` (`item_id`, `location_id`, `no_of_items`, `curr_condition`) VALUES
-(108, 105, 10, 'New'),
-(109, 106, 20, 'New');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `items`
 --
 
@@ -93,60 +72,18 @@ CREATE TABLE `items` (
   `item_description` varchar(50) NOT NULL,
   `item_purchase_price` decimal(10,0) NOT NULL,
   `item_purchase_year` year(4) NOT NULL,
-  `item_depreciation_rate` decimal(10,0) NOT NULL
+  `item_depreciation_rate` decimal(10,0) NOT NULL,
+  `image` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`item_id`, `item_name`, `item_description`, `item_purchase_price`, `item_purchase_year`, `item_depreciation_rate`) VALUES
-(7, 'Table', 'Four-legged', 1000, '2012', 17),
-(8, 'AC', 'Onida 3 star', 125000, '2022', 21),
-(9, 'Desktop', 'Intel systems', 200000, '2012', 21);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `items_info`
---
-
-CREATE TABLE `items_info` (
-  `item_id` int(11) NOT NULL,
-  `item_name` varchar(20) NOT NULL DEFAULT '"NOT ENTERED"',
-  `description` varchar(50) NOT NULL DEFAULT '"EMPTY"',
-  `depreciation_rate` decimal(10,0) NOT NULL DEFAULT 0,
-  `purchase_year` year(4) NOT NULL DEFAULT current_timestamp(),
-  `purchase_value` decimal(10,0) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `items_info`
---
-
-INSERT INTO `items_info` (`item_id`, `item_name`, `description`, `depreciation_rate`, `purchase_year`, `purchase_value`) VALUES
-(108, 'Desktop', 'Intel systems', 10, '2024', 23),
-(109, 'Fan', 'Ceiling Fan', 21, '2022', 2340);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `location`
---
-
-CREATE TABLE `location` (
-  `location_id` int(11) NOT NULL,
-  `location_name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `location`
---
-
-INSERT INTO `location` (`location_id`, `location_name`) VALUES
-(104, 'GFL'),
-(105, 'SFL'),
-(106, 'FFL');
+INSERT INTO `items` (`item_id`, `item_name`, `item_description`, `item_purchase_price`, `item_purchase_year`, `item_depreciation_rate`, `image`) VALUES
+(7, 'Table', 'Four-legged', 1000, '2012', 17, ''),
+(10, 'Fan', 'three legged fan', 3450, '2024', 12, ''),
+(13, 'AC', '5 star Onida', 2000, '2024', 12, '');
 
 -- --------------------------------------------------------
 
@@ -209,37 +146,20 @@ CREATE TABLE `tracker` (
 --
 
 INSERT INTO `tracker` (`location_id`, `item_id`, `count_working`, `count_defect`) VALUES
-(2, 7, 13, 4),
-(3, 7, 10, 10);
+(3, 7, 10, 10),
+(5, 7, 40, 40),
+(1, 13, 20, 20),
+(5, 10, 15, 15);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `availability`
---
-ALTER TABLE `availability`
-  ADD KEY `ITEM_ID_FK` (`item_id`),
-  ADD KEY `LOCATION_ID_FK` (`location_id`);
-
---
 -- Indexes for table `items`
 --
 ALTER TABLE `items`
   ADD PRIMARY KEY (`item_id`);
-
---
--- Indexes for table `items_info`
---
-ALTER TABLE `items_info`
-  ADD PRIMARY KEY (`item_id`);
-
---
--- Indexes for table `location`
---
-ALTER TABLE `location`
-  ADD PRIMARY KEY (`location_id`);
 
 --
 -- Indexes for table `locations`
@@ -268,19 +188,7 @@ ALTER TABLE `tracker`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `items_info`
---
-ALTER TABLE `items_info`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
-
---
--- AUTO_INCREMENT for table `location`
---
-ALTER TABLE `location`
-  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `locations`
@@ -297,13 +205,6 @@ ALTER TABLE `login_details`
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `availability`
---
-ALTER TABLE `availability`
-  ADD CONSTRAINT `ITEM_ID_FK` FOREIGN KEY (`item_id`) REFERENCES `items_info` (`item_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `LOCATION_ID_FK` FOREIGN KEY (`location_id`) REFERENCES `location` (`location_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `tracker`
